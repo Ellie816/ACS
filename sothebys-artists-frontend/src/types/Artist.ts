b85aa0533016f6ac059bb67a3354abe57c82a008
Elinor Holt <elliemarsan@gmail.com>
2025-07-21 14:29:53 +0100

    Delete sothebys-artists-frontend directory

--- sothebys-artists-frontend/src/types/Artist.ts
+++
@@ -1,8 +0,0 @@
-export interface Artist {
-    artist_name: string;
-    title: string;
-    estimate: string;
-    lot_number: number;
-    collecting_society?: string;
-    resale_right: boolean;
-}
